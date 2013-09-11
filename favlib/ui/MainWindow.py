954699a232368d536ec30bf26ccd07b6bcded4cf
Christopher Smith <chris@x86-64.info>
2013-09-12 03:24:45 +0900

    MainWindow working

--- favlib/ui/MainWindow.py
+++ favlib/ui/MainWindow.py
@@ -2,5 +2,7 @@ from PyQt4 import QtGui, QtCore
 from favlib.ui.Ui_MainWindow import Ui_MainWindow
 
 class MainWindow(QtGui.QMainWindow):
-	def __init__(self):
-		QtGui.QMainWindow.__init__(self)
+	def __init__(self, parent = None):
+		QtGui.QWidget.__init__(self, parent)
+		self.ui = Ui_MainWindow()
+		self.ui.setupUi(self)
